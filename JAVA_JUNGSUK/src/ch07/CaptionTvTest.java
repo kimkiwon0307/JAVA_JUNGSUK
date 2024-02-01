78bbbd5ec788a32ab5b383845edc12882de9b448
Kim Kiwon <kkwkkj@naver.com>
2024-02-01 10:59:48 +0900

    ch07 상속의 정의와 장점

---
+++ JAVA_JUNGSUK/src/ch07/CaptionTvTest.java
@@ -0,0 +1,40 @@
+package ch07;
+
+class Tv {
+	boolean power;
+	int channel;
+	
+	void power() { power = !power; }
+	void channelUp() { ++channel;}
+	void channelDwon() { --channel;}
+}
+
+class CaptionTv extends Tv{
+	
+	boolean caption;
+	void displayCaption(String text) {
+		if(caption) {
+			System.out.println("자막기능을 켰습니다.");
+			System.out.println(text);
+		}else {
+			System.out.println("자막기능이 꺼져있습니다.");
+		}
+	}
+}
+
+class CaptionTvTest{
+	public static void main(String[] args) {
+		
+		CaptionTv ctv = new CaptionTv();
+		ctv.channel=10;
+		ctv.channelUp();
+		System.out.println(ctv.channel);
+		ctv.displayCaption("HI, World");
+		ctv.caption = true;
+		ctv.displayCaption("Hello, World");
+		
+		
+	}
+	
+	
+}
