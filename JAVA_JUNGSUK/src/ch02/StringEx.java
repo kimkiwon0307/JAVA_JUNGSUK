27614dd8697044bcc7641e59b827d9062d00afe2
Kim Kiwon <kkwkkj@naver.com>
2024-02-06 12:14:40 +0900

    2024-02-06 상수와 리터럴

---
+++ JAVA_JUNGSUK/src/ch02/StringEx.java
@@ -0,0 +1,24 @@
+package ch02;
+
+public class StringEx {
+	public static void main(String[] args) {
+		
+		String name = "JA" + "va";
+		String str = name + 8.0;
+		
+		System.out.println(name);
+		System.out.println(str);
+		System.out.println(7 + " ");
+		System.out.println(" " + 7);
+		
+		System.out.println(7+"");
+		System.out.println("" + 7);
+		System.out.println("" + "");
+		System.out.println(7 + 7 + "");
+		System.out.println("" + 7 + 7);
+		
+	}
+}
+
+
+
